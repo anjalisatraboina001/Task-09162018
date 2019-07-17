92497b800f4f390392a8f0dd72482cfc700a4756
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-16 19:55:50 -0600

    Update EmployeeController.java

--- simplified/src/main/java/org/springframework/hateoas/examples/EmployeeController.java
+++ simplified/src/main/java/org/springframework/hateoas/examples/EmployeeController.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.springframework.hateoas.examples;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
@@ -35,11 +21,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Spring Web {@link RestController} used to generate a REST API.
- *
- * @author Greg Turnquist
- */
+
 @RestController
 class EmployeeController {
 
@@ -84,12 +66,7 @@ class EmployeeController {
 		}
 	}
 
-	/**
-	 * Look up a single {@link Employee} and transform it into a REST resource. Then return it through Spring Web's
-	 * {@link ResponseEntity} fluent API.
-	 *
-	 * @param id
-	 */
+	
 	@GetMapping("/employees/{id}")
 	ResponseEntity<EntityModel<Employee>> findOne(@PathVariable long id) {
 
@@ -101,13 +78,7 @@ class EmployeeController {
 				.orElse(ResponseEntity.notFound().build());
 	}
 
-	/**
-	 * Update existing employee then return a Location header.
-	 * 
-	 * @param employee
-	 * @param id
-	 * @return
-	 */
+	
 	@PutMapping("/employees/{id}")
 	ResponseEntity<?> updateEmployee(@RequestBody Employee employee, @PathVariable long id) {
 
