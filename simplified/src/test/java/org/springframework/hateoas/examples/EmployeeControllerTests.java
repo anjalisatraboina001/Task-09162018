21c68c3358a4e8913e75671c712f4bdebbc54183
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-26 15:43:02 -0600

    Update EmployeeControllerTests.java

--- simplified/src/test/java/org/springframework/hateoas/examples/EmployeeControllerTests.java
+++ simplified/src/test/java/org/springframework/hateoas/examples/EmployeeControllerTests.java
@@ -41,18 +41,12 @@ public class EmployeeControllerTests {
 				.andExpect(status().isOk()) //
 				.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
 				.andExpect(jsonPath("$._embedded.employees[0].id", is(1)))
-				.andExpect(jsonPath("$._embedded.employees[0].firstName", is("Frodo")))
-				.andExpect(jsonPath("$._embedded.employees[0].lastName", is("Baggins")))
-				.andExpect(jsonPath("$._embedded.employees[0].role", is("ring bearer")))
+				.andExpect(jsonPath("$._embedded.employees[0].firstName", is("anjali")))
+				.andExpect(jsonPath("$._embedded.employees[0].lastName", is("manish")))
+				.andExpect(jsonPath("$._embedded.employees[0].role", is("navya")))
 				.andExpect(jsonPath("$._embedded.employees[0]._links.self.href", is("http://localhost/employees/1")))
 				.andExpect(jsonPath("$._embedded.employees[0]._links.employees.href", is("http://localhost/employees")))
-				.andExpect(jsonPath("$._embedded.employees[1].id", is(2)))
-				.andExpect(jsonPath("$._embedded.employees[1].firstName", is("Bilbo")))
-				.andExpect(jsonPath("$._embedded.employees[1].lastName", is("Baggins")))
-				.andExpect(jsonPath("$._embedded.employees[1].role", is("burglar")))
-				.andExpect(jsonPath("$._embedded.employees[1]._links.self.href", is("http://localhost/employees/2")))
-				.andExpect(jsonPath("$._embedded.employees[1]._links.employees.href", is("http://localhost/employees")))
-				.andExpect(jsonPath("$._links.self.href", is("http://localhost/employees"))) //
+			
 				.andReturn();
 	}
 }
